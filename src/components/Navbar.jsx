0df1d9b80e427111a56e3c0f6281214170e87da8
M. Naufal Fawwaz Haryono <96192431+NaufalFawwaz@users.noreply.github.com>
2023-12-13 21:35:27 +0700

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         <>
             <nav className='flex justify-between px-10 py-5 gap-14 sticky'>
                 <Link to={'Home'} className='flex text-2xl items-center gap-3 pl-20'>
-                    <img src={logo} alt="logo" className='' />
+                    <img src={logo} alt="logo" className='w-14' />
                     <h1 className='text-main font-black'>M. Naufal Fawwaz Haryono</h1>
                 </Link>
                 <ul className='flex items-center list-none gap-11 pr-36 text-xl'>
