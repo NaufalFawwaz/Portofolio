8bd514f66e35e9db42c76562339144a9a506df0d
M. Naufal Fawwaz Haryono <96192431+NaufalFawwaz@users.noreply.github.com>
2023-12-16 23:31:42 +0700

    new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,10 @@ import { Link } from 'react-router-dom';
 import AboutMe from './AboutMe';
 
 const Home = () => {
+    const theButtonClick = () => {
+        const driveLink = 'https://drive.google.com/file/d/1SDwYm9vCKu345BDgDvx1JgNhqhkz2Bo-/view?usp=sharing';
+        window.open(driveLink, '_blank');
+    };
     return (
         <main className="max-w-7xl mx-auto">
             <section className='md:h-[85vh] md:max-h-[500px] mt-16 flex flex-row items-center justify-between w-full'>
@@ -26,15 +30,15 @@ const Home = () => {
                         />
                         <p className='pb-3 text-3xl'>Welcome to my portfolio website</p>
                     </div>
-                    <button className='bg-gray-800 rounded-[12px] p-3 text-white font-black w-fit mt-9'>Find Out More About Me</button>
+                    <button onClick={theButtonClick} className='bg-gray-800 rounded-[12px] p-3 text-white font-black w-fit mt-9 hover:bg-black hover:text-eeeeee hover:scale-110'>Find Out More About Me</button>
                     <div className='flex flex-row gap-7 pt-16'>
-                        <Link to={'https://github.com/NaufalFawwaz'}>
+                        <Link to={'https://github.com/NaufalFawwaz'} target='_blank' rel='noopener noreferrer' className='icon-link github'>
                             <FaGithub className='w-12 h-12'/>
                         </Link>
-                        <Link to={'https://www.linkedin.com/in/m-naufal-fawwaz-haryono-1a09a9291/'}>
+                        <Link to={'https://www.linkedin.com/in/m-naufal-fawwaz-haryono-1a09a9291/'} target='_blank' rel='noopener noreferrer' className='icon-link linkedin'>
                             <FaLinkedinIn className='w-12 h-12'/>
                         </Link>
-                        <Link to={'https://www.instagram.com/naufal2738/'}>
+                        <Link to={'https://www.instagram.com/naufal2738/'} target='_blank' rel='noopener noreferrer' className='icon-link instagram'>
                             <FaInstagram className='w-12 h-12'/>
                         </Link>
                     </div>
